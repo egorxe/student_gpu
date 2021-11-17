32323318def262bc56d2ea9a105ad8aca019bdb9
Egor Lukyanchenko <13577050+egorxe@users.noreply.github.com>
2021-11-17 20:42:00 +0300

    Add more convertion funcs

--- src/rtl/pkg/gpu_pkg.vhd
+++ src/rtl/pkg/gpu_pkg.vhd
@@ -48,9 +48,14 @@ constant GPU_PIPE_CMD_FRAME_END     : vec32 := X"FFFFFF01";
 function tf(x : real) return float32;                                   -- alias with shorter name for real->float32
 function to_slv(a : integer; size : natural) return std_logic_vector;   -- integer->std_logic_vector
 function to_vec32(a : integer) return std_logic_vector;                 -- integer->std_logic_vector 32 bit
+function to_vec32(a : float32) return std_logic_vector;                 -- float32->std_logic_vector 32 bit
 function zero_vec(size : integer) return std_logic_vector;              -- create vector of all zeroes
+
 function to_uint(a : std_logic_vector) return integer;                  -- std_logic_vector->unsigned integer
+function to_uint(a : float32) return integer;                           -- float32->unsigned integer
 function to_sint(a : std_logic_vector) return integer;                  -- std_logic_vector->signed integer
+
+--function to_str(a : std_logic_vector) return integer;                   -- std_logic_vector->signed integer
 
 end gpu_pkg;
 
@@ -76,6 +81,11 @@ begin
     return to_slv(a, 32);
 end;
 
+function to_vec32(a : float32) return std_logic_vector is
+begin
+    return std_logic_vector(to_unsigned(a, 32));
+end;
+
 function zero_vec(size : integer) return std_logic_vector is
 begin
     return to_slv(0, size);
@@ -84,6 +94,11 @@ end;
 function to_uint(a : std_logic_vector) return integer is
 begin
     return to_integer(unsigned(a));
+end;
+
+function to_uint(a : float32) return integer is
+begin
+    return to_uint(to_vec32(a));
 end;
 
 function to_sint(a : std_logic_vector) return integer is
