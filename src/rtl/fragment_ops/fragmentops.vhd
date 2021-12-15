8a081e598921a781dc5701aa0b756e5ce15bc7ca
Nikita-Presnov <presnovnikita@yandex.ru>
2021-12-15 21:41:44 +0300

    axis-stream template

--- src/rtl/fragment_ops/fragmentops.vhd
+++ src/rtl/fragment_ops/fragmentops.vhd
@@ -1,10 +1,7 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
-library std;
-use std.textio.all;
-use ieee.std_logic_textio.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.std_logic_unsigned.all;
 
 use work.gpu_pkg.all;
 use work.file_helper_pkg.all;
