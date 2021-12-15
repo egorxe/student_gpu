dae09b8ccc4941975aad28197c2bb35dfe5ae4c4
Nadezhda <noma99970@gmail.com>
2021-12-15 17:31:18 +0300

    Добава снтезруемы код растерзатора

--- src/rtl/pkg/gpu_pkg.vhd
+++ src/rtl/pkg/gpu_pkg.vhd
@@ -36,6 +36,34 @@ package gpu_pkg is
     type M44 is array (0 to 3) of V4;
     type FV4 is array (0 to 3) of float32;
     type FM44 is array (0 to 3) of FV4;
+    
+    type rast_vertex is record
+        x : float32;
+        y : float32;
+        z : float32;
+        w : float32;
+        r : float32;
+        g : float32;
+        b : float32;
+        a : float32;
+    end record;
+    
+    type RV is array (0 to 2) of rast_vertex;
+    
+    type ef is record
+        x   : float32;
+        y   : float32;
+        a0x : float32;
+        a0y : float32;
+        a1x : float32;
+        a1y : float32;
+        a2x : float32;
+        a2y : float32;
+        w0  : float32;
+        w1  : float32;
+        w2  : float32;
+        mask : integer;
+    end record;
 
     ------------------------------------------------------------------------
     ---------------------------- CONSTANTS2 --------------------------------
@@ -45,6 +73,7 @@ package gpu_pkg is
     constant GPU_PIPE_CMD_POLY_VERTEX : vec32 := X"FFFFFF00";
     constant GPU_PIPE_CMD_FRAME_END   : vec32 := X"FFFFFF01";
     constant GPU_PIPE_CMD_FRAGMENT    : vec32 := X"FFFFFF02";
+    constant PIPELINE_MAX_Z           : vec32 := X"00010000";
 
 
 
@@ -52,15 +81,23 @@ package gpu_pkg is
     ---------------------------- FUNCTIONS ---------------------------------
     ------------------------------------------------------------------------
 
-    function tf(x          : real) return float32;                             -- alias with shorter name for real->float32
-    function to_slv(a      : integer; size : natural) return std_logic_vector; -- integer->std_logic_vector
-    function to_vec32(a    : integer) return std_logic_vector;                 -- integer->std_logic_vector 32 bit
-    function to_vec32(a    : float32) return std_logic_vector;                 -- float32->std_logic_vector 32 bit
-    function zero_vec(size : integer) return std_logic_vector;                 -- create vector of all zeroes
+    function tf(x : real) return float32; -- alias with shorter name for real->float32
+
+    function to_slv(a          : integer; size : natural) return std_logic_vector;      -- integer->std_logic_vector
+    function to_s_slv(intValue : integer; vecLength : integer) return std_logic_vector; -- integer->std_logic_vector
+    function to_slv(value      : real) return std_logic_vector;                         -- real->std_logic_vector
+
+    function to_vec32(a        : integer) return std_logic_vector; -- integer->std_logic_vector 32 bit
+    function to_vec32(a        : float32) return std_logic_vector; -- float32->std_logic_vector 32 bit
 
     function to_uint(a : std_logic_vector) return integer; -- std_logic_vector->unsigned integer
     function to_uint(a : float32) return integer;          -- float32->unsigned integer
     function to_sint(a : std_logic_vector) return integer; -- std_logic_vector->signed integer
+
+    function to_real(value : std_logic_vector) return real; -- std_logic_vector->real
+
+    function zero_vec(size     : integer) return std_logic_vector; -- create vector of all zeroes
+    function max_vec(vecLength : integer) return std_logic_vector; -- create vector of all ones
 
     --function to_str(a : std_logic_vector) return integer;                   -- std_logic_vector->signed integer
 
@@ -73,15 +110,34 @@ end gpu_pkg;
 
 package body gpu_pkg is
 
+    ----------------------------------to float32------------------------------------------------
+
     function tf(x : real) return float32 is
     begin
         return to_float(x);
     end;
 
+    ----------------------------------to std_logic_vector------------------------------------------------
+
     function to_slv(a : integer; size : natural) return std_logic_vector is
     begin
         return std_logic_vector(to_unsigned(a, size));
     end;
+
+    function to_slv(value : real
+        ) return std_logic_vector is
+    begin
+        return to_slv(tf(value));
+    end function;
+
+    function to_s_slv(intValue : integer;
+            vecLength : integer
+        ) return std_logic_vector is
+    begin
+        return std_logic_vector(to_signed(intValue, vecLength));
+    end function;
+
+    ---------------------------------to vec32------------------------------------------------
 
     function to_vec32(a : integer) return std_logic_vector is
     begin
@@ -93,10 +149,8 @@ package body gpu_pkg is
         return std_logic_vector(to_unsigned(a, 32));
     end;
 
-    function zero_vec(size : integer) return std_logic_vector is
-    begin
-        return to_slv(0, size);
-    end;
+    ----------------------------------to integer------------------------------------------------
+
 
     function to_uint(a : std_logic_vector) return integer is
     begin
@@ -112,5 +166,26 @@ package body gpu_pkg is
     begin
         return to_integer(signed(a));
     end;
+
+    ----------------------------------to real------------------------------------------------
+
+    function to_real(value : std_logic_vector
+        ) return real is
+    begin
+        return to_real(to_float(value));
+    end function;
+
+    -----------------------------------helpers------------------------------------------
+
+    function zero_vec(size : integer) return std_logic_vector is
+    begin
+        return to_slv(0, size);
+    end;
+
+    function max_vec(vecLength : integer
+        ) return std_logic_vector is
+    begin
+        return to_s_slv(-1, vecLength);
+    end function;
 
 end gpu_pkg;
